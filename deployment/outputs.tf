4f0b622f436f7bdb4fe0daab25c8e5fdae0b5607
wwalpha <wwalpha@gmail.com>
2019-10-06 01:04:16 +0900

    add output

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -30,3 +30,40 @@ output "deployment_id" {
 output "created_date" {
   value = aws_api_gateway_deployment.this.created_date
 }
+id - The internal id assigned to this domain name by API Gateway.
+# ------------------------------------------------------------------------------------------------
+# The creation date of the deployment
+# ------------------------------------------------------------------------------------------------
+output "created_date" {
+  value = aws_api_gateway_deployment.this.created_date
+}
+# ------------------------------------------------------------------------------------------------
+# The upload date associated with the domain certificate.
+# ------------------------------------------------------------------------------------------------
+output "certificate_upload_date" {
+  value = aws_api_gateway_deployment.this.created_date
+}
+# ------------------------------------------------------------------------------------------------
+# The hostname created by Cloudfront to represent the distribution that implements this domain name mapping.
+# ------------------------------------------------------------------------------------------------
+output "cloudfront_domain_name" {
+  value = aws_api_gateway_domain_name.this.cloudfront_domain_name
+}
+# ------------------------------------------------------------------------------------------------
+# For convenience, the hosted zone ID (Z2FDTNDATAQYW2) that can be used to create a Route53 alias record for the distribution.
+# ------------------------------------------------------------------------------------------------
+output "cloudfront_zone_id" {
+  value = aws_api_gateway_domain_name.this.cloudfront_zone_id
+}
+# ------------------------------------------------------------------------------------------------
+# The hostname for the custom domain's regional endpoint.
+# ------------------------------------------------------------------------------------------------
+output "regional_domain_name" {
+  value = aws_api_gateway_domain_name.this.regional_domain_name
+}
+# ------------------------------------------------------------------------------------------------
+# The hosted zone ID that can be used to create a Route53 alias record for the regional endpoint.
+# ------------------------------------------------------------------------------------------------
+output "regional_zone_id" {
+  value = aws_api_gateway_domain_name.this.regional_zone_id
+}
