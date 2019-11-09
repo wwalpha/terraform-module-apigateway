6c48af0650114360ac71c8b0a39913a38a3958a9
wwalpha <wwalpha@gmail.com>
2019-11-09 16:19:34 +0900

    bug fix

--- method/permission.tf
+++ method/permission.tf
@@ -4,9 +4,10 @@
 resource "aws_lambda_permission" "lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = local.function_arn
+  function_name = local.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${local.execution_arn}/*/${var.http_method}${local.resource_path}"
+  qualifier     = local.function_alias
 
   count = var.integration_type == "AWS_PROXY" ? 1 : 0
 }
