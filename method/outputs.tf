0a4f6e145e2135314c925afbda9db29c44cd8148
wwalpha <wwalpha@gmail.com>
2019-12-09 11:23:30 +0900

    test fix

--- method/outputs.tf
+++ method/outputs.tf
@@ -2,5 +2,5 @@ output "method_id" {
   value = aws_api_gateway_method.this.id
 }
 output "integration_id" {
-  value = aws_api_gateway_integration.lambda[0].id
+  value = length(aws_api_gateway_integration.lambda) != 0 ? aws_api_gateway_integration.lambda[0].id : null
 }
