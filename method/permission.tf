0b226851b60d86442ec535240345f550c9b67d9f
wwalpha <wwalpha@gmail.com>
2019-12-09 11:38:22 +0900

    remove lambda permission function

--- method/permission.tf
+++ method/permission.tf
@@ -1,13 +1,13 @@
-# ------------------------------
-# AWS Lambda Role Permission
-# ------------------------------
-resource "aws_lambda_permission" "lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = local.function_arn
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${local.execution_arn}/*/${var.http_method}${local.resource_path}"
-  qualifier     = local.function_alias
+# # ------------------------------
+# # AWS Lambda Role Permission
+# # ------------------------------
+# resource "aws_lambda_permission" "lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = local.function_arn
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${local.execution_arn}/*/${var.http_method}${local.resource_path}"
+#   qualifier     = local.function_alias
 
-  count = var.integration_type == "AWS_PROXY" ? 1 : 0
-}
+#   count = var.integration_type == "AWS_PROXY" ? 1 : 0
+# }
